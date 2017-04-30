21ba2839b72eb95bf17295d03e576601d36b0ca6
harry2636 <harry2636@naver.com>
2017-04-30 21:17:42 +0900

    Implemented multiple track management function. Now able to add other tracks dynamically.

--- public/js/audioRenderer.js
+++ public/js/audioRenderer.js
@@ -1,39 +1,68 @@
-var wavesurfer;
+var wavesurferList = [];
 
 $(document).ready(function() {
-	wavesurfer = WaveSurfer.create({
-	    container: '#waveform',
-	    waveColor: 'violet',
-    	progressColor: 'purple',
-    	//barWidth: 2,
-    	cursorWidth: 2,
-    	//height: 300,
-    	//splitChannels: true,
+    wavesurferList.push(getEmptyContainer());
+    wavesurferList[0].load("/tracks/Inuyasha_gayagm.mp3");
+    wavesurferList.push(getEmptyContainer());
+    wavesurferList[1].load("/tracks/electric_romeo.mp3");
+
+    $("#addRow").on("click", addTrackRow)
+});
+
+function addTrackRow() {
+    var waveformNum = wavesurferList.length;
+    var newRowtag = `
+        <div>
+            <div id="waveRow${waveformNum}">
+                <div>
+                    <input type="file" id="upload${waveformNum}" accept = "audio/*"/> 
+                    <button id="play${waveformNum}"> 재생/일시정지 </button>
+                    <button id="stop${waveformNum}"> 정지 </button>
+                    <button id="download${waveformNum}"> 다운로드 </button>
+                </div>
+            </div>
+            <div id="waveform-timeline${waveformNum}"></div>
+        </div>
+    `
+
+    $("#waveContent").append(newRowtag);
+    wavesurferList.push(getEmptyContainer());
+}
+
+function getEmptyContainer() {
+    var waveformNum = wavesurferList.length;
+    var wsInstance = WaveSurfer.create({
+        container: "#waveRow" + waveformNum,
+        waveColor: 'violet',
+        progressColor: 'purple',
+        //barWidth: 2,
+        cursorWidth: 2,
+        //height: 300,
+        //splitChannels: true,
         //scrollParent: true
-	});
-    wavesurfer.load('/tracks/Inuyasha_gayagm.mp3');
-	wavesurfer.on('ready', function () {
-	    wavesurfer.play();
+    });
+    wsInstance.on('ready', function () {
+        //wsInstance.play();
         var timeline = Object.create(WaveSurfer.Timeline);
         timeline.init({
-            wavesurfer: wavesurfer,
-            container: '#waveform-timeline'
+            wavesurfer: wsInstance,
+            container: '#waveform-timeline' + waveformNum
         });
 
-        wavesurfer.enableDragSelection({});
-	});
-
-	$("#play").click(function() {
-		wavesurfer.playPause();
-	});
-
-	$("#stop").click(function() {
-		wavesurfer.stop();
-	});
-	$("#download").click(function() {
-		saveToWav(wavesurfer.backend.buffer);
-	});
-    $("#upload").change(function() {
-        wavesurfer.loadBlob(this.files[0]);
+        wsInstance.enableDragSelection({});
     });
-});
+    $("#play" + waveformNum).click(function() {
+        wsInstance.playPause();
+    });
+
+    $("#stop" + waveformNum).click(function() {
+        wsInstance.stop();
+    });
+    $("#download" + waveformNum).click(function() {
+        saveToWav(wsInstance.backend.buffer);
+    });
+    $("#upload" + waveformNum).change(function() {
+        wsInstance.loadBlob(this.files[0]);
+    });
+    return wsInstance
+}
