f6708cee31c57d20d24f46b4a370d2e8a1bae61a
JaeminShin <999fg@kaist.ac.kr>
2017-05-11 20:57:00 +0900

    GridFS and REST API POST upload works very well

--- server-app/app.js
+++ server-app/app.js
@@ -1,26 +1,17 @@
-var express = require('express');
+var mongoose = require("mongoose");
+var express = require("express");
+var bodyparser = require("body-parser");
 var app = express();
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-mongoose.connect('mongodb://52.79.87.203:27017/mongodb_tutorial');
+mongoose.connect("mongodb://52.79.87.203:27017/mongodb_tutorial");
+var conn = mongoose.connection;
 
 var port = process.env.PORT || 3004;
-var fs = require('fs');
-var Grid = require('gridfs-stream');
-var conn = mongoose.connection;
-var router = require('./routes')(app, fs, Grid, conn, mongoose);
-
-conn.on('error', console.error);
-conn.once('open', function(){
-    console.log("Connected to mongod server");
-});
-
-var router = require('./routes')(app, fs, Grid, conn);
 
 var server = app.listen(port, function(){
     console.log("Express server has started on port " + port)
 });
+
+app.use(bodyparser.json());
+
+var router = require('./routes')(app, mongoose, conn)
