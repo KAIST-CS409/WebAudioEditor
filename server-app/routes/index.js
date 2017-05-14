a058e73f883cce4f5eff5997700a7dff0758c8ea
Jaemin Shin <999fg@kaist.ac.kr>
2017-05-15 02:15:04 +0900

    uploadAudio / downloadAudio / deleteAudio / getAudioList implemented

--- server-app/routes/index.js
+++ server-app/routes/index.js
@@ -16,11 +16,97 @@ module.exports = function(app, mongoose, conn)
                 });
                 fs.createReadStream(files.file.path).pipe(writestream);
             }
+            if (err) {
+                res.status(404).json({
+                    status: 404,
+                    message: 'Upload Error'
+                });
+            }
         });
-        form.on('end', function () {
-            res.send('Completed ... go check fs.files & fs.chunks in mongodb');
+        form.on('end', function (err) {
+            if (err) {
+                res.status(404).json({
+                    status: 404,
+                    message: 'Upload Error'
+                });
+            } else {
+                res.status(200).json({
+                    status: 200,
+                    message: 'Upload Successful'
+                });
+            }
         });
     });
-    app.get('/api/downloadAudio', function (req, res) {
+    app.post('/api/downloadAudio', function (req, res) {
+        grid.mongo = mongoose.mongo;
+        var gfs = grid(conn.db);
+        var aid = req.body["audio_id"];
+        gfs.exist({ _id: aid }, function(err, found) {
+            if (err) {
+                handleError(err); 
+                res.status(404).json({
+                    status: 404,
+                    message: 'Download Error'
+                });
+            } else if (!found) {
+                res.send('Error on the database looking for the file.' + aid)
+                res.status(404).json({
+                    status: 404,
+                    message: 'File does not exist'
+                });
+            } else {
+                gfs.createReadStream({ _id: aid }).pipe(res);
+            }
+        });
+    });
+
+    app.post('/api/deleteAudio', function (req, res) {
+        grid.mongo = mongoose.mongo;
+        var gfs = grid(conn.db);
+        var aid = req.body["audio_id"];
+        gfs.exist({ _id: aid }, function(err, found) {
+            if (err) {
+                handleError(err); 
+                res.status(404).json({
+                    status: 404,
+                    message: 'Delete Error'
+                });
+            } else if (!found) {
+                res.send('Error on the database looking for the file.' + aid)
+                res.status(404).json({
+                    status: 404,
+                    message: 'File does not exist'
+                });
+            } else {
+                gfs.remove({ _id: aid }, function (err) {
+                    if (err) {
+                        res.status(404).json({
+                            status: 404,
+                            message: 'Delete Error'
+                        });
+                    } else {
+                        res.status(200).json({
+                            status: 200,
+                            message: 'Delete Successful'
+                        });
+                    }
+                });
+            }
+        });
+    });
+
+    app.get('/api/getAudioList', function (req, res) {
+        conn.db.collection('fs.files').find().toArray(function(err, items) {
+            if (err) {
+                res.status(404).json({
+                    status: 404,
+                    message: 'Error'
+                });
+            } else {
+                var response = {};
+                var status = {status: 200};
+                res.status(200).json(Object.assign(response, items, status));
+            }
+        });
     });
 }
